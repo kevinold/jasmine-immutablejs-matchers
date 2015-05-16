91b2c59fbe9c94530b4f646861df6244c6e5556b
Kevin Old <kevin@kevinold.com>
2015-05-16 10:54:51 -0500

    fix test and matchers

--- __tests__/jasmine-immutablejs-matchers-tests.js
+++ __tests__/jasmine-immutablejs-matchers-tests.js
@@ -2,30 +2,38 @@ jest.dontMock('immutable');
 var Immutable = require('immutable');
 require('../src/jasmine-immutablejs-matchers');
 
-describe("isImmutable", function() {
+describe("jasmine immutable matchers", function() {
+  
+  var data;
+  var immutableData;
 
-  describe("when invoked", function() {
+  beforeEach(function() {
+    data = {'a': 1, 'b': 2};
+    immutableData = Immutable.Map(data);
+  });
+
+  describe("toBeImmutable", function() {
+
+    it("should confirm Immutability", function() {
+      expect(immutableData).toBeImmutable();
+    });
+
+  });
+
+  describe("toBeActualImmutable", function() {
 
     describe("when expected Immutable object is actual Immutable object", function() {
 
-      it("should confirm Immutability", function() {
-        var data = Immutable.Map({'a': 1, 'b': 2});
-        expect(data).toBeImmutable();
-      });
-
-      it("should confirm", function() {
-        var data = Immutable.Map({'a': 1, 'b': 2});
-        expect(data).is(data);
+      it("should confirm expected is actual", function() {
+        expect(data).toBeActualImmutable(data);
       });
 
       it("should be false when NOT a match", function() {
-        var obj = {'a': 1, 'b': 2};
-        var data = Immutable.Map(obj);
-        expect(data).not.is(obj);
+        expect(immutableData).not.toBeActualImmutable(data);
       });
 
     });
 
   });
-});
 
+});  
--- src/jasmine-immutablejs-matchers.js
+++ src/jasmine-immutablejs-matchers.js
@@ -3,15 +3,15 @@ var Immutable = require('immutable');
 beforeEach(function () {
 
   this.addMatchers({
+
     toBeImmutable: function() {
       return Immutable.Iterable.isIterable(this.actual);
+    },
+
+    toBeActualImmutable: function(expected) {
+      return Immutable.is(this.actual, expected);
     }
-    is: function(expected) {
-      if (this.toBeImmutable(expected) && this.toBeImutable(this.actual)) {
-        return Immutable.is(this.actual, expected);
-      }
-      return false;
-    }
+
   });
 
 });
